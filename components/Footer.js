b8760eff21532fb92994ddb6801589398d3da092
zafeerahamed <emailzafir@gmail.com>
2024-10-09 19:25:38 +0400

    Some Corrections made for styling

--- components/Footer.js
+++ components/Footer.js
@@ -11,36 +11,36 @@ const Footer = () => {
 
   return (
     <footer id="contact" class="bg-blue-950 text-white py-8">
-      <div class="container mx-auto px-4">
-        <div class="flex flex-wrap justify-between">
-          <div class="w-full md:w-1/3 mb-6 md:mb-0">
-            <h5 class="text-lg font-bold mb-4">Contact Me</h5>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-between">
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h5 className="text-lg font-bold mb-4">Contact Me</h5>
             <ul>
             <li><p>Dubai, United Arab Emirates</p></li>
             <li><p>Phone: +971 52 531 0747</p></li>
             <li><a className="decoration-none" href="mailto:emailzafir@gmail.com">Email: emailzafir@gmail.com</a></li></ul>
           </div>
-          <div class="w-full md:w-1/3 mb-6 md:mb-0">
-            <h5 class="text-lg font-bold mb-4">Follow Me</h5>
-            <div class="flex space-x-4">
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h5 className="text-lg font-bold mb-4">Follow Me</h5>
+            <div className="flex space-x-4">
               <ul>
-                <li><a href="https://www.linkedin.com/in/zafeerahamed" class="text-white hover:text-gray-400">LinkedIn</a></li>
-                <li><a href="https://www.github.com/zafeerahamed" class="text-white hover:text-gray-400">Github</a></li>
-                <li><a href="https://www.x.com/zafeer_ahamed" class="text-white hover:text-gray-400">Twitter</a></li>
-                <li><a href="https://www.instagram.com/urzafeer" class="text-white hover:text-gray-400">Instagram</a></li>
+                <li><a href="https://www.linkedin.com/in/zafeerahamed" className="text-white hover:text-gray-400">LinkedIn</a></li>
+                <li><a href="https://www.github.com/zafeerahamed" className="text-white hover:text-gray-400">Github</a></li>
+                <li><a href="https://www.x.com/zafeer_ahamed" className="text-white hover:text-gray-400">Twitter</a></li>
+                <li><a href="https://www.instagram.com/urzafeer" className="text-white hover:text-gray-400">Instagram</a></li>
               </ul>
             </div>
           </div>
-          <div class="w-full md:w-1/3">
-            <h5 class="text-lg font-bold mb-4">Quick Links</h5>
+          <div className="w-full md:w-1/3">
+            <h5 className="text-lg font-bold mb-4">Quick Links</h5>
             <ul>
-              <li><button onClick={() => scrollToSection('home')} class="text-white hover:text-gray-400">Home</button></li>
-              <li><button onClick={() => scrollToSection('about')} class="text-white hover:text-gray-400">About</button></li>
-              <li><button onClick={() => scrollToSection('portfolio')} class="text-white hover:text-gray-400">Portfolio</button></li>
+              <li><button onClick={() => scrollToSection('home')} className="text-white hover:text-gray-400">Home</button></li>
+              <li><button onClick={() => scrollToSection('about')} className="text-white hover:text-gray-400">About</button></li>
+              <li><button onClick={() => scrollToSection('portfolio')} className="text-white hover:text-gray-400">Portfolio</button></li>
             </ul>
           </div>
         </div>
-        <div class="mt-8 text-center">
+        <div className="mt-8 text-center">
           <p>© 2024 Zafeer Ahamed. All rights reserved.</p>
         </div>
       </div>
